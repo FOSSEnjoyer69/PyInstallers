fb01ff3f4e685fae71a4a1cde719388894ca631c
Jacob Mars <fossenjoyer@startmail.com>
2025-05-15 03:40:53 +1000

    added python3.13

---
+++ Linux/Debian/install python3.13.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+
+sudo apt install -y python3.13 python3.13-venv python3.13-dev
